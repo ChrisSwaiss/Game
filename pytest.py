a6e927b47236ba33bc94f7de21edf0a65e9320e2
ChrisSwaiss <44216238+ChrisSwaiss@users.noreply.github.com>
2018-12-23 18:28:06 -0800

    Update pytest.py
    added movement capabilities, still rudimentary

--- pytest.py
+++ pytest.py
@@ -1,5 +1,30 @@
-for i in range(10):
-    for j in range(10):
-	    print("0 ", end=" "),
-    print("")
-#just prints a 10x10 grid of 0's, do smth with this later
+pos = [0]*10, [0]*10
+x, y = 1, 1
+
+def drawmap(x, y):
+	for i in range(10):
+		for j in range(10):
+			if i == y and j == x:
+				print ("x", end = " ")
+			else:
+				print ("0", end = " ")
+		print("")
+
+drawmap(x, y)
+while True:
+	movement = input("Please enter W, S, A, or D")
+	if movement.upper() == 'A':
+		x -= 1
+	elif movement.upper() == 'S':
+		y += 1
+	elif movement.upper() == 'D':
+		x += 1
+	elif movement.upper() == 'W':
+		y -= 1
+	#elif movement.upper() == 'EXIT':
+		#close game
+
+	drawmap(x, y)
+
+#limit movement to map, figure out how to make exit command work,
+#dont redraw map every time
